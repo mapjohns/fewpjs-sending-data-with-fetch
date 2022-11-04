59750daaba8cc030f49e7b98837355718d78215d
Matthew <mattj20@gmail.com>
2022-11-04 16:59:52 -0700

    commit tree e3a5c16caeb421a65a30c3aa76a9aaefc7e8e84b

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`
+        })
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(object) {
+        let id = document.createElement('h2');
+        id.innerHTML = object.id
+        document.body.append(id)
+      })
+    .catch(function(error) {
+    let errMessage = document.createElement('h3')
+    errMessage.innerHTML = error.message
+    document.body.append(errMessage)
+    });
+}
